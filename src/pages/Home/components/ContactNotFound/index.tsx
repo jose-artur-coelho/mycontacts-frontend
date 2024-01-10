bc96bf90471464a20f171521ee512a0c0b9fd54c
José Artur <jose.artur.coelho@ccc.ufcg.edu.br>
2024-01-10 12:28:02 -0300

    style: update structure for ContactNotFound component

--- src/pages/Home/components/ContactNotFound/index.tsx
+++ src/pages/Home/components/ContactNotFound/index.tsx
@@ -1,15 +1,16 @@
 import magnifier from "../../../../assets/images/icons/magnifier.svg";
+import { ContactNotFoundContainer } from "./styles";
 
 interface ContactNotFoundProps {
   searchTerm: string;
 }
 export default function ContactNotFound({ searchTerm }: ContactNotFoundProps) {
   return (
-    <div>
+    <ContactNotFoundContainer>
       <img src={magnifier} alt="Magnifier with question mark" />
       <span>
         Nenhum resultado foi encontrado para <strong>"{searchTerm}".</strong>
       </span>
-    </div>
+    </ContactNotFoundContainer>
   );
 }
