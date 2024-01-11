d25bba4901affc5e707f0ef27c83a1c75f1c2178
José Artur <jose.artur.coelho@ccc.ufcg.edu.br>
2024-01-10 21:12:53 -0300

    style: change writing of Contact interface

---
+++ src/types/Contact.ts
@@ -0,0 +1,8 @@
+export default interface Contact {
+  id: string;
+  name: string;
+  email: null | string;
+  phone: null | string;
+  category_name: null | string;
+  category_id: null | string;
+}
