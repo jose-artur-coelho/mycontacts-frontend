6245bb8c1c2f133728b01b3aba9b7faeae322a08
José Artur <jose.artur.coelho@ccc.ufcg.edu.br>
2024-01-15 14:39:31 -0300

    feat: makeRequest method

--- src/services/utils/HttpClient.ts
+++ src/services/utils/HttpClient.ts
@@ -1,40 +1,55 @@
 import APIError from "../../errors/APIError";
 
+interface Options {
+  method: string;
+  body?: object;
+  headers?: object;
+}
+
 class HttpClient {
   baseURL;
   constructor(baseURL: string) {
     this.baseURL = baseURL;
   }
-  async get(path: string) {
-    const response = await fetch(`${this.baseURL}${path}`);
+  get(path: string, options?: { headers: object }) {
+    return this.MakeRequest(path, {
+      method: "GET",
+      headers: options?.headers,
+    });
+  }
 
+  post(path: string, options: { body: object; headers?: object }) {
+    return this.MakeRequest(path, {
+      method: "POST",
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+  async MakeRequest(path: string, options: Options) {
+    const headers = new Headers();
+
+    if (options.body) headers.append("Content-Type", "application/json");
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([name, value]) =>
+        headers.append(name, value)
+      );
+    }
+
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: options.method,
+      headers,
+      body: JSON.stringify(options.body),
+    });
+
+    let body = null;
     const contentType = response.headers.get("Content-Type");
-
     if (contentType?.includes("application/json")) {
-      const body = await response.json();
+      body = await response.json();
       if (response.ok) return body;
       throw new APIError(body.error);
     } else {
       throw new APIError(`${response.status} - ${response.statusText}`);
-    }
-  }
-
-  async post(path: string, data: object) {
-    const response = await fetch(`${this.baseURL}${path}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      const body = await response.json();
-      return body;
-    }
-    if (response.status >= 400 && response.status < 500) {
-      const body = await response.json();
-      throw new APIError(body.error);
     }
   }
 }
