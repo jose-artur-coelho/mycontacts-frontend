748b368cc98f231b231b9a2d15c3e8687469bfca
José Artur <jose.artur.coelho@ccc.ufcg.edu.br>
2024-01-19 08:10:45 -0300

    feat: add getContactById method

--- src/services/ContactsService.ts
+++ src/services/ContactsService.ts
@@ -1,14 +1,19 @@
 import HttpClient from "./utils/HttpClient";
 
 class ContactService {
-  httpClient;
+  private httpClient;
   constructor() {
     this.httpClient = new HttpClient("http://localhost:3000");
   }
-  async listContacts(orderBy = "asc") {
+  listContacts(orderBy = "asc") {
     return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
-  async createContact(options: { body: object; headers?: object }) {
+
+  getContactbyId(id: string) {
+    return this.httpClient.get(`/contacts/${id}`);
+  }
+
+  createContact(options: { body: object; headers?: object }) {
     return this.httpClient.post(`/contacts`, options);
   }
 }
